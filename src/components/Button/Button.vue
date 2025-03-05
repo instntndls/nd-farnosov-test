26f9c2bdd5dad0a0af66d9d5b5daee47d0e8623e
instntndls <farn7229@gmail.com>
2025-03-05 21:28:16 +0500

    Initial commit

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+import { computed } from "vue";
+
+interface Props {
+  label?: string;
+  icon?: string;
+  round?: boolean;
+  disabled?: boolean;
+}
+
+const props = defineProps<Props>();
+const emit = defineEmits(["click"]);
+
+const buttonClass = computed(() => [
+  "base-button",
+  { round: props.round, disabled: props.disabled }
+]);
+</script>
+
+<template>
+  <button :class="buttonClass" :disabled="disabled" @click="emit('click')">
+    <img v-if="icon" :src="icon" alt="icon" class="icon" />
+    <p v-if="label" class="text-normal">
+      {{ label }}
+    </p>
+  </button>
+</template>
+
+<style scoped>
+.base-button {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
+  height: 56px;
+  padding: 0 24px;
+  background: var(--color-green-light);
+  border: none;
+  border-radius: 32px;
+  color: white;
+  text-align: center;
+  white-space: nowrap;
+  cursor: pointer;
+  transition: 0.2s;
+}
+
+.base-button.round {
+  width: 56px;
+  height: 56px;
+  justify-content: center;
+}
+
+.base-button:hover {
+  background: var(--color-green-middle);
+}
+
+.base-button:active {
+  background: var(--color-green-dark);
+}
+
+.base-button.disabled {
+  background: var(--color-gray);
+  cursor: not-allowed;
+}
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+import Button from '@/components/Button/Button.vue'
+</script>
+
+<template>
+  <div class="container">
+    <div class="">
+      <img src="/src/assets/icons/logo.svg" alt="logo" class="logo">
+      <img src="/src/assets/icons/Group.svg" alt="logo" class="group">
+    </div>
+
+    <header class="header">
+      <Button label="Вход" icon="/src/assets/icons/login.svg"></Button>
+      <Button style="background-color: var(--color-middle)" label="" round icon="/src/assets/icons/user.svg"></Button>
+    </header>
+    <header class="header">
+      <Button label="Вход" disabled icon="/src/assets/icons/login.svg"></Button>
+      <Button label="" disabled round icon="/src/assets/icons/close.svg"></Button>
+    </header>
+    <a href="asdasd">Зарегистрируйтесь</a>
+    <h1 class="title">
+      Мои заметки
+    </h1>
+    <h2>
+      Heading H2
+    </h2>
+    <h3>
+      Heading H3
+    </h3>
+    <h4>
+      Heading H4
+    </h4>
+    <p class="text-normal">
+      Text normal
+    </p>
+    <p class="text-small">
+      Text small
+    </p>
+    <p class="text-small-bold">
+      Text small bold
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  width: 218.29px;
+  height: 50px;
+}
+.title {
+  width: 208px;
+}
+header {
+  display: flex;
+  justify-content: space-between;
+  width: fit-content;
+  gap: 24px;
+}
+</style>
+
+
